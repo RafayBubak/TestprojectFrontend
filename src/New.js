6fc8544bcba9999a28c61fca40511967e1f3c2de
MuhammadAkram69 <awais6925@gmail.com>
2023-02-15 02:23:50 +0500

    commit after design modification

--- src/New.js
+++ src/New.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BiCategory } from "react-icons/bi";
 
 export default function New() {
   return (
@@ -45,12 +46,44 @@ export default function New() {
 
     <div className="main-container">
 
-        <div className="container">
+      <div className="container">
 
-            <div className="left-c-button">Categories</div>
-            <div className="linput-div"> Searchbar</div>
-            <div className="left-c-button">Seach-button</div>
+        <div className="left-c-button">
+          <i className="icon"> <BiCategory/></i>
+          <span className="Catgtext">Categories</span>
         </div>
+        <div className="input-div">
+            <input
+              className="inputfield"
+              placeholder="What are you looking for?">
+            </input>
+
+            <select className="SearchSelection">
+               <option value="">All Categories</option>
+               <option classNmae="Catdrop" value="ppe">
+                 {" "}
+                 PPE{" "}
+               </option>
+               <option className="Catdrop" value="wellness">
+                 {" "}
+                 Wellness{" "}
+               </option>
+               <option className="Catdrop" value="home_decor">
+                 {" "}
+                 Home Decor{" "}
+               </option>
+               <option className="Catdrop" value="miscellaneous">
+                 {" "}
+                Miscellaneous{" "}
+               </option>
+           </select>
+
+        </div>
+        <div className="searchbutton">
+            <span class="">Search</span>
+            <i class=""></i>
+        </div>
+      </div>
     </div>
 
 
